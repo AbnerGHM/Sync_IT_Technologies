e47956fbbb0e086ed89a6b7f126ce1b926f3370a
AnsiosoForever <abner.ghm@hotmail.com>
2020-09-02 22:54:19 -0500

    Initial project

---
+++ app/src/main/java/com/abner/syncittechnologies/login/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package com.abner.syncittechnologies.login.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.abner.syncittechnologies.login.data.LoginDataSource
+import com.abner.syncittechnologies.login.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                loginRepository = LoginRepository(
+                    dataSource = LoginDataSource()
+                )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
